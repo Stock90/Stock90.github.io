8db31e9f5bc30f144c27c3f488bbb9a562bab260
Stock90 <sergio.salamanca.90@gmail.com>
2024-09-13 13:30:37 -0500

    Update crown.js

--- crown.js
+++ crown.js
@@ -3,24 +3,23 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4100;
-let precio = [726.52	,
-    985.43	,
-    1305.57	,
-    273.85	,
-    309.94	,
-    (2087600/1.19/dolar)	,
-    912.73	,
-    309.28	,
-    374.37	,
-    498.89	,
-    568.84	
+let dolar = 4150;
+let precio = [	882.21	,
+1196.60	,
+1585.34	,
+332.53	,
+364.65	,
+497.32	,
+1108.31	,
+363.86	,
+440.44	,
+586.92	,
+669.23	
 ];
-
 
 let pesos = [];
 
 for (let i = 0; i < precio.length; i++) {
-    pesos[i] = formatter.format(Math.ceil(((precio[i] / 0.82) * dolar * 1.19) / 100) * 100);
+    pesos[i] = formatter.format(Math.ceil(((precio[i] / 1) * dolar * 1.19) / 100) * 100);
     document.getElementById("precio" + i).innerHTML = pesos[i];
 } 
