9b04527b0355468f9a48629650a426f69035a77e
Stock90 <sergio.salamanca.90@gmail.com>
2024-09-13 16:28:21 -0500

    Update soundcraft.js

--- soundcraft.js
+++ soundcraft.js
@@ -3,22 +3,22 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4100;
-let precio = [(1487500/1.19/dolar),
-    536.81,
-    (4080000/1.19/dolar),
-    956.72,
-    882.50,
-    (299280/1.19/dolar),
-    129.78,
-    391.76,
-    256.87
+let dolar = 4150;
+let precio = [354.36
+651.84
+971.96
+1161.73
+938.54
+70.47
+150.05
+475.71
+311.92
     ];
 
 let pesos = [];
 
 for (let i = 0; i < precio.length; i++) {
-    pesos[i] = formatter.format(Math.ceil(((precio[i] / 0.82) * dolar * 1.19) / 100) * 100);
+    pesos[i] = formatter.format(Math.ceil(((precio[i] / 1) * dolar * 1.19) / 100) * 100);
     document.getElementById("precio" + i).innerHTML = pesos[i];
 }
 
