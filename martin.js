ea0c69a540ccf6e257067053a6a2fd29a37f4c2a
Stock90 <sergio.salamanca.90@gmail.com>
2024-09-13 16:16:43 -0500

    Update martin.js

--- martin.js
+++ martin.js
@@ -3,7 +3,7 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4100;
+let dolar = 4150;
 let precio = [8902.52	,
     5092.43	,
     5609.40	,
