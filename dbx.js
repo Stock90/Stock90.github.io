52b903d1d1c0811844717f500f359d8ca4cf2c23
Stock90 <sergio.salamanca.90@gmail.com>
2024-10-18 10:39:00 -0500

    Update dbx.js

--- dbx.js
+++ dbx.js
@@ -3,7 +3,7 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4150;
+let dolar = 4200;
 let precio = [198.46	,
 311.11	,
 43.54	,
