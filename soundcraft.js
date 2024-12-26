8c31b9410a59e0fa3125e896d0f6e71df2f3386b
Stock90 <sergio.salamanca.90@gmail.com>
2024-12-26 16:20:44 -0500

    Update soundcraft.js

--- soundcraft.js
+++ soundcraft.js
@@ -13,7 +13,8 @@ let precio = [358.39,
 let pesos = [];
 
 for (let i = 0; i < precio.length; i++) {
-    pesos[i] = formatter.format(Math.ceil(((precio[i] / 1) * dolar * 1.19) / 100) * 100);
+    pesos[i] = formatter.format(Math.ceil(((precio[i] / 0.82) * dolar * 1.19) / 100) * 100);
     document.getElementById("precio" + i).innerHTML = pesos[i];
+    
 }
 
