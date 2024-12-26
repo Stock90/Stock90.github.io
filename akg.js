fd772463e937064d8a148ffc78dcaa2040fed45b
Stock90 <sergio.salamanca.90@gmail.com>
2024-12-26 15:17:07 -0500

    Update akg.js

--- akg.js
+++ akg.js
@@ -3,7 +3,7 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4300;
+let dolar = 4350;
 let precio = [17.37	,
 36.08	,
 70.35	,
