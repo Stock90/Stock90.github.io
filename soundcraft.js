2d704099dc6d742e418b13a616ce3f9f6738ae98
Stock90 <sergio.salamanca.90@gmail.com>
2025-01-30 10:07:33 -0500

    Update soundcraft.js

--- soundcraft.js
+++ soundcraft.js
@@ -3,11 +3,14 @@ const formatter = new Intl.NumberFormat('en-US', {
     currency: 'COP',
     minimumFractionDigits: 0
 })
-let dolar = 4350;
-let precio = [358.39,
-993.63,
-956.72,
-527.43
+let dolar = 4250;
+let precio = [105.56,
+              137.09,
+              254.37,
+    354.91,
+    983.99,
+    947.43,
+    527.43
     ];
 
 let pesos = [];
